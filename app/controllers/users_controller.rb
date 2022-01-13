2c95b11558d4bad83dc75da1c1ccf436308ca332
bluespex <piyush.shandilya1999@gmail.com>
2022-01-13 18:08:20 +0530

    signup feature added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    def new
+        @user  = User.new
+    end
+
+    def create
+        puts user_params.inspect
+        @user = User.new(user_params)
+        
+        puts @user.inspect
+        puts @user.save.inspect
+        if @user.save
+            log_in(@user)
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to (root_path)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :name)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,10 @@ class User < ApplicationRecord
     has_secure_password
 
     has_one :profile, dependent: :destroy
+
+    after_create :build_profile
+
+    def build_profile
+        Profile.create(user: self) # Associations must be defined correctly for this syntax, avoids using ID's directly.
+    end
 end
